e39f71072dde24677cc7c38a381f0b43fb7d798a
Ernesto Ledesma <eeledesma@teco.com.ar>
2024-06-10 09:28:03 -0300

    final del proyecto

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,20 +1,52 @@
+import  { useEffect, useState } from 'react'
+import { GifGridItem } from './GifGridItem'
+
 
 
 export const GifGrid = ({ category }) => {
-    key='aca va la key de usuario de giphy'
+
+const [images, setImages] = useState([])  
+
+useEffect(() => {
+  getGifs( category)
+}, [])
+
+
+    const key='H34PGip1gUHR5Uzn7j9B5hUP30a4qELj&'
+
     const getGifs = async() => {
-        const url = `https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=${key}&`
+        const url = `https://api.giphy.com/v1/gifs/search?q=${category}&limit=10&api_key=${key}&`
         const resp = await fetch(url)
         const { data } = await resp.json()
-        console.log(data)
+     
+        const gifs = data.map( img => {
+          return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
 
-    }
+    })
+    console.log(gifs)
+    setImages(gifs)
 
-    getGifs()
+  }
+
+
 
   return (
-    <div>
+    <div className='card-grid'>
         <h3>{category}</h3>
+        <ol>
+            {
+                images.map( img => (
+                    <GifGridItem
+                        key={img.id}
+                        {...img}
+                     />
+                ))
+            }
+        </ol>
     </div>
   )
 }
